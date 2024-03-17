01282f545f029e25d7b2a65c4fdcb8356dc8742b
Ali Shokoohi <shokoohi.dev@gmail.com>
2024-03-17 23:43:52 +0330

    Sendign transactions and waiting for confim

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/ali-shokoohi/anchor-go-tic-tac-toe/pkg/generated/tic_tac_toe"
 	"github.com/gagliardetto/solana-go"
 	"github.com/gagliardetto/solana-go/rpc"
+	confirm "github.com/gagliardetto/solana-go/rpc/sendAndConfirmTransaction"
+	"github.com/gagliardetto/solana-go/rpc/ws"
 	"github.com/pkg/errors"
 )
 
@@ -20,11 +22,16 @@ type KeyPairs struct {
 }
 
 func main() {
+	ctx := context.TODO()
 	// Create a new RPC client:
 	cluster := rpc.DevNet_RPC
+	wsCluster := rpc.DevNet_WS
 	client := rpc.New(cluster)
-
-	_ = client
+	// Create a new WS client (used for confirming transactions)
+	wsClient, err := ws.Connect(ctx, wsCluster)
+	if err != nil {
+		log.Fatal("Failed at connection into websocket")
+	}
 
 	keyPairs, err := loadKeyPairs()
 	if err != nil {
@@ -32,36 +39,70 @@ func main() {
 		return
 	}
 
-	tSetupGame := tic_tac_toe.NewSetupGameInstruction(
-		keyPairs.PlayerTwoPrivateKey.PublicKey(),
-		keyPairs.GamePrivateKey.PublicKey(),
-		keyPairs.PlayerOnePrivateKey.PublicKey(),
-		keyPairs.GamePrivateKey.PublicKey(),
-	)
-	tile := tic_tac_toe.Tile{Row: 3, Column: 3}
-	tPlay := tic_tac_toe.NewPlayInstruction(tile, tSetupGame.GetGameAccount().PublicKey, *tSetupGame.PlayerTwo)
-	_ = tPlay
 	programID, err := solana.PublicKeyFromBase58(ProgramID)
 	if err != nil {
 		log.Fatal("Failed at getting programID: ", err)
 	}
 	tic_tac_toe.SetProgramID(programID)
 
-	recent, err := client.GetRecentBlockhash(context.TODO(), rpc.CommitmentFinalized)
+	tSetupGame := tic_tac_toe.NewSetupGameInstruction(
+		keyPairs.PlayerTwoPrivateKey.PublicKey(),
+		keyPairs.GamePrivateKey.PublicKey(),
+		keyPairs.PlayerOnePrivateKey.PublicKey(),
+		solana.SystemProgramID,
+	)
+
+	recent, err := client.GetRecentBlockhash(ctx, rpc.CommitmentFinalized)
 	if err != nil {
 		log.Fatal("Failed at getting recent block Hash: ", err)
 		return
 	}
-	tx, err := solana.NewTransaction([]solana.Instruction{tSetupGame.Build()}, recent.Value.Blockhash, solana.TransactionPayer(keyPairs.PlayerOnePrivateKey.PublicKey()))
+	tx, err := solana.NewTransaction(
+		[]solana.Instruction{tSetupGame.Build()},
+		recent.Value.Blockhash,
+		solana.TransactionPayer(keyPairs.GamePrivateKey.PublicKey()),
+	)
 	if err != nil {
 		log.Fatal("Failed at calling SetupGame transaction: ", err)
 		return
 	}
 	log.Println("SetupGame tx:", tx)
+	signers := []solana.PrivateKey{keyPairs.GamePrivateKey, keyPairs.PlayerOnePrivateKey}
+	_, err = tx.Sign(
+		func(key solana.PublicKey) *solana.PrivateKey {
+			for _, signer := range signers {
+				if signer.PublicKey().Equals(key) {
+					return &signer
+				}
+			}
+			return nil
+		})
+	if err != nil {
+		log.Fatal("unable to sign SetupGame transaction: ", err)
+		return
+	}
+
+	err = tx.VerifySignatures()
+	if err != nil {
+		log.Fatal("Error at Verifying tx SetupGame signatures: ", err)
+		return
+	}
+	// Send transaction, and wait for confirmation:
+	sig, err := confirm.SendAndConfirmTransaction(
+		ctx,
+		client,
+		wsClient,
+		tx,
+	)
+	if err != nil {
+		log.Fatal("Failed at sending and confirm SetupGame transaction: ", err)
+		return
+	}
+	log.Println("SetupGame Sig: ", sig)
 }
 
 func loadKeyPairs() (KeyPairs, error) {
-	gamePrivateKey, err := solana.PrivateKeyFromSolanaKeygenFile("./config/keypairs/game_id.json")
+	gamePrivateKey, err := solana.PrivateKeyFromSolanaKeygenFile("./config/keypairs/game2_id.json")
 	if err != nil {
 		err = errors.New("Failed at getting gamePrivateKey: " + err.Error())
 		return KeyPairs{}, err
